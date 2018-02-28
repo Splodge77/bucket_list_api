aa42b86742ad1049902d4e8ff7aa479eb65a7768
Max Robson <maxwell.robson@gmail.com>
2018-02-28 12:34:12 +0000

    init, server, webpack etc constructed

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
+const MongoClient = require('mongodb').MongoClient;
+// const ObjectID = require('mongodb').ObjectID;
+
+
+server.use(parser.json());
+server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client){
+  if(err) {
+    console.log(err);
+    return;
+  }
+  const db = client.db('bucket_list');
+  console.log('Connected to database');
+});
